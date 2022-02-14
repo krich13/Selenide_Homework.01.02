04d71c86355d04b33a17ffab32cb59b2640b733a
Krsitina Klimenchenko <klimenchenko.kri.ch@gmail.com>
2022-02-14 15:55:39 +0300

    Tests fix

--- src/test/java/ru/netology/ApplicationFormTests.java
+++ src/test/java/ru/netology/ApplicationFormTests.java
@@ -2,10 +2,14 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -18,45 +22,61 @@ public class ApplicationFormTests {
     @Test
     void shouldRegisterIfAllConditionsCorrect() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
         $("[placeholder='Город']").setValue("Красноярск");
         $("[name='name']").setValue("Иван Петров-Водкин");
         $("[name='phone']").setValue("+79119418502");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(Selectors.withText("Успешно!"))
+                .should(Condition.appear, Duration.ofSeconds(15));
+        String actualResult = $(Selectors.withText("Встреча успешно забронирована на")).getText();
+        Assertions.assertTrue(actualResult.contains(LocalDate.now().plusDays(3).format(formatter)));
     }
 
     @Test
     void correctTestCitySelectsByClick() { //Хочу проверить, можно ли выбрать город из списка
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
-        $(Selectors.byAttribute("type", "text")).setValue("Мо");
-        $(Selectors.byText("Москва")).click();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
+        $(byAttribute("type", "text")).setValue("Мо");
+        $(byText("Москва")).click();
         $("[name='name']").setValue("Иван Петров-Водкин");
         $("[name='phone']").setValue("+79119418502");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        $(Selectors.withText("Успешно!"))
+                .should(Condition.appear, Duration.ofSeconds(15));
+        String actualResult = $(Selectors.withText("Встреча успешно забронирована на")).getText();
+        Assertions.assertTrue(actualResult.contains(LocalDate.now().plusDays(3).format(formatter)));
     }
 
     //Негативные тесты
+
     @Test
     void shouldFailWithLatinInName() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
         $("[placeholder='Город']").setValue("Красноярск");
-        $("[name='name']").setValue("Ivan Petrov");
-        $("[name='phone']").setValue("+79119418502");
+        $("[name='name']").setValue("Ivan Petrov-Vodkin");
+        $("[name='phone']").setValue("+791194185021");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $(Selectors.withText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.")).should(Condition.appear, Duration.ofSeconds(15));
+        $(Selectors.withText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы")).should(Condition.appear, Duration.ofSeconds(15));
+
     }
 
     @Test
     void shouldFailWithLatinInCity() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
         $("[placeholder='Город']").setValue("Moscow");
         $("[name='name']").setValue("Иван Петров-Водкин");
         $("[name='phone']").setValue("+79119418502");
@@ -68,19 +88,24 @@ public class ApplicationFormTests {
     @Test
     void shouldFailWithLongPhoneNumber() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
         $("[placeholder='Город']").setValue("Красноярск");
         $("[name='name']").setValue("Иван Петров-Водкин");
         $("[name='phone']").setValue("+791194185021");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
         $(Selectors.withText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")).should(Condition.appear, Duration.ofSeconds(15));
+
     }
 
     @Test
     void shouldFailWithShortPhoneNumber() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
         $("[placeholder='Город']").setValue("Красноярск");
         $("[name='name']").setValue("Иван Петров-Водкин");
         $("[name='phone']").setValue("+7911");
@@ -92,7 +117,9 @@ public class ApplicationFormTests {
     @Test
     void shouldFailWithPhoneNumberInCorrectFormat() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
         $("[placeholder='Город']").setValue("Красноярск");
         $("[name='name']").setValue("Иван Петров-Водкин");
         $("[name='phone']").setValue("89119418502");
@@ -104,7 +131,9 @@ public class ApplicationFormTests {
     @Test
     void shouldFailWithUncheckedBox() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
         $("[placeholder='Город']").setValue("Красноярск");
         $("[name='name']").setValue("Иван Петров-Водкин");
         $("[name='phone']").setValue("+79119418502");
@@ -117,7 +146,9 @@ public class ApplicationFormTests {
     @Test
     void shouldFailWithEmptyName() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
         $("[placeholder='Город']").setValue("Красноярск");
         $("[name='name']").setValue(" ");
         $("[name='phone']").setValue("+79119418502");
@@ -128,7 +159,9 @@ public class ApplicationFormTests {
     @Test
     void shouldFailWithEmptyCity() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
         $("[placeholder='Город']").setValue(" ");
         $("[name='name']").setValue("Иван Петров-Водкин");
         $("[name='phone']").setValue("+79119418502");
@@ -139,7 +172,9 @@ public class ApplicationFormTests {
     @Test
     void shouldFailWithEmptyPhone() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[placeholder='Дата встречи']").append(LocalDate.now().plusDays(3).format(formatter)).pressEnter();
         $("[placeholder='Город']").setValue("Красноярск");
         $("[name='name']").setValue("Иван Петров-Водкин");
         $("[name='phone']").setValue("  ");
@@ -150,7 +185,8 @@ public class ApplicationFormTests {
     @Test
     void shouldFailWithEmptyFields() {
         open("http://localhost:9999");
-        $("[placeholder='Дата встречи']").setValue(String.valueOf(futureDate));
+        open("http://localhost:9999");
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[placeholder='Город']").setValue(" ");
         $("[name='name']").setValue(" ");
         $("[name='phone']").setValue(" ");
